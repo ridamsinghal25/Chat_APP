3b35b2eba27282262aae255307a40247f68bc5e0
Ridam-Singhal <manusinghal84@gmail.com>
2024-04-07 19:09:48 +0530

    added auth middleware

---
+++ Backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,33 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import jwt from "jsonwebtoken";
+
+export const verifyJWT = asyncHandler(async (req, _, next) => {
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
+
+    if (!token) {
+      new ApiError(404, "Invalid token");
+    }
+
+    const decodedToken = await jwt.verify(
+      token,
+      process.env.ACCESS_TOKEN_SCERET
+    );
+
+    const user = await User.findById(decodedToken._id).select(
+      "-password -refreshToken"
+    );
+
+    if (!user) {
+      new ApiError(401, "Invalid Access token");
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    new ApiError(401, error?.message || "Invalid access token");
+  }
+});
