07e37075f2dff45373c58111823f0c70e831a45b
ridamsinghal25 <manusinghal84@gmail.com>
2024-04-09 19:11:52 +0530

    add pre hook to my user models

---
+++ Backend/src/models/user.models.js
@@ -0,0 +1,60 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: [true, "full name is required"],
+      trim: true,
+    },
+    username: {
+      type: String,
+      required: [true, "username is required"],
+      trim: true,
+      unique: true,
+      lowercase: true,
+    },
+    email: {
+      type: String,
+      required: [true, "email is required"],
+      trim: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: [true, "password is required"],
+      trim: true,
+    },
+    avatar: {
+      type: {
+        public_id: {
+          type: String,
+          default: "",
+        },
+        url: {
+          type: String,
+          default: "default_avatar.png",
+        },
+        localPath: {
+          type: String,
+          default: "",
+        },
+      },
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+export const User = mongoose.model("User", userSchema);
