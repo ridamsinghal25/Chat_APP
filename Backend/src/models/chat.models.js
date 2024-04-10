51106e0cdcca88c65843f8e3c48c0206ce816d1a
ridamsinghal25 <manusinghal84@gmail.com>
2024-04-10 19:10:49 +0530

    add chat model to codebase

---
+++ Backend/src/models/chat.models.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const chatSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    isGroupChat: {
+      type: Boolean,
+      default: false,
+    },
+    lastMessage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
+    },
+    participants: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Chat = mongoose.model("Chat", chatSchema);
