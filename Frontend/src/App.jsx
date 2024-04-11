a3c4e3b20e77225e38741ba98a3db831710863ea
ridamsinghal25 <manusinghal84@gmail.com>
2024-04-11 15:12:45 +0530

    add feature to get user in app.jsx to the codebase

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,39 @@
+import { useEffect, useState } from "react";
 import { Header } from "./comonents";
 import { Outlet } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { login, logout } from "./slices/authSlice";
+import { Loader } from "./comonents/index";
+import authService from "./freeapi/auth";
 
 function App() {
-  return (
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          const user = userData.data.data;
+          dispatch(login({ user }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+  console.log("hello");
+
+  return !loading ? (
     <>
       <Header />
-      <div>
+      <main>
         <Outlet />
-      </div>
+      </main>
     </>
+  ) : (
+    <Loader />
   );
 }
 
