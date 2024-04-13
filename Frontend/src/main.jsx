944b71a685886820e2d5e531ad696623fb60fa0b
ridamsinghal25 <manusinghal84@gmail.com>
2024-04-13 16:57:03 +0530

    remove redux persist as dependency from codebase

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,8 +5,6 @@ import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store.js";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { PersistGate } from "redux-persist/integration/react";
-import { persistStore } from "redux-persist";
 import SignupPage from "./pages/SignupPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import Home from "./pages/Home.jsx";
@@ -47,14 +45,10 @@ const router = createBrowserRouter([
   },
 ]);
 
-const persistor = persistStore(store);
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <RouterProvider router={router} />
-      </PersistGate>
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
--- Frontend/src/store.js
+++ Frontend/src/store.js
@@ -1,44 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./slices/authSlice";
-import storage from "redux-persist/lib/storage/session";
-import { persistReducer } from "redux-persist";
-import { combineReducers } from "@reduxjs/toolkit";
-import { createTransform } from "redux-persist";
-import { encryptData, decryptData } from "./utils/encryptAndDecrypt";
-
-// might create a problem of serializable check
-
-const myTransforms = createTransform(
-  function (inboundState) {
-    const modifiedState = {
-      ...inboundState,
-      userData: encryptData(inboundState.userData),
-    };
-    return modifiedState;
-  },
-  function (outboundState) {
-    const modifiedState = {
-      ...outboundState,
-      userData: decryptData(outboundState.userData),
-    };
-    return modifiedState;
-  }
-);
-
-const persistConfig = {
-  key: "root",
-  storage,
-  // transforms: [myTransforms],
-};
-
-const reducer = combineReducers({
-  auth: authSlice,
-});
-
-const persistedReducer = persistReducer(persistConfig, reducer);
 
 const store = configureStore({
-  reducer: persistedReducer,
+  reducer: {
+    auth: authSlice,
+  },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
