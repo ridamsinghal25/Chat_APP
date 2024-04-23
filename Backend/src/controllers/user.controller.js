3fc650ac4ac91200f960694729d18bd1e8081779
ridamsinghal25 <manusinghal84@gmail.com>
2024-04-23 15:00:54 +0530

    add controller to register user to the codebase

---
+++ Backend/src/controllers/user.controller.js
@@ -0,0 +1,70 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { User } from "../models/user.models.js";
+import { uploadToCloudinary } from "../utils/cloudinary.js";
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { fullName, username, email, password } = req.body;
+
+  if (
+    [fullName, username, email, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const userExists = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (userExists) {
+    throw new ApiError(400, "User with email or username already exists");
+  }
+
+  let avatarLocalPath;
+  console.log("req.files: ", req.files);
+
+  if (
+    req.files &&
+    Array.isArray(req.files.avatar) &&
+    req.files.avatar.length > 0
+  ) {
+    avatarLocalPath = req.files.avatar[0].path;
+  }
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is required");
+  }
+
+  const avatar = await uploadToCloudinary(avatarLocalPath);
+
+  if (!avatar) {
+    throw new ApiError(400, "Error while uploading avatar file");
+  }
+
+  const user = await User.create({
+    fullName,
+    email,
+    password,
+    avatar: {
+      public_id: avatar.public_id,
+      url: avatar.url,
+      localPath: avatarLocalPath,
+    },
+    username: username.toLowerCase(),
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while registering user");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User registered successfully"));
+});
+
+export { registerUser };
